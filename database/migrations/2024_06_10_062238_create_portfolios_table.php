85e80989e25abb3dc41ff051998a9e5b4894999f
soya0721 <sotyannpc21@gmail.com>
2024-06-10 16:02:59 +0900

    mypage見れるようにしました

--- database/migrations/2024_06_10_062238_create_portfolios_table.php
+++ database/migrations/2024_06_10_062238_create_portfolios_table.php
@@ -11,17 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-// ここに追加
-            $table->varchar('bio');
-            $table->varchar('photo');
-
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->varchar();
             $table->timestamps();
         });
     }
@@ -31,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('portfolios');
     }
 };
