a7b06717b7ae6fd3a03425d1113cd26dd8ce247e
sousuke-code <162286022+sousuke-code@users.noreply.github.com>
2024-06-10 22:24:19 +0900

    マルチログイン機能の実装

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Company\CompanyLoginController;
+use App\Http\Controllers\Company\CompanyRegisterController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +31,28 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::group(['prefix' => 'company'], function () {
+    // 登録
+    Route::get('register', [CompanyRegisterController::class, 'create'])
+        ->name('company.register');
+
+    Route::post('register', [CompanyRegisterController::class, 'store']);
+
+    // ログイン
+    Route::get('login', [CompanyLoginController::class, 'showLoginPage'])
+        ->name('company.login');
+
+    Route::post('login', [CompanyLoginController::class, 'login']);
+
+    // 以下の中は認証必須のエンドポイントとなる
+    Route::middleware(['auth:company'])->group(function () {
+        // ダッシュボード
+        Route::get('dashboard', fn() => view('company.dashboard'))
+            ->name('company.dashboard');
+    });
 });
 
 require __DIR__.'/auth.php';
