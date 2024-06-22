23787e1ea29e6e39b79a221314a935e0f4fe6570
soya0721 <sotyannpc21@gmail.com>
2024-06-22 11:17:03 +0900

    検索機能実装（言語のみ）

--- app/Http/Controllers/Company/CompanyController.php
+++ app/Http/Controllers/Company/CompanyController.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Company;
-
+use App\Models\User;
+use App\Models\UserLanguages;
+use App\Models\ProgrammingLanguage;
 class CompanyController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-
         return view('company.profile');
     }
 
@@ -21,5 +22,26 @@ class CompanyController extends Controller
         // $company = Company::find($id);
         return view('company.edit');
     }
+    public function search(Request $request)
+    {
+        $language = $request->input('language');
 
+
+        $query = User::query();
+
+        if ($language !== 'All') {
+            $query->whereHas('userLanguages', function($q) use ($language) {
+                $q->where('programming_language', $language);
+            });
+        }
+
+
+
+        $users = $query->get();
+
+        return view('company.search_results', ['users' => $users]);
+    }
 }
+
+    
+
