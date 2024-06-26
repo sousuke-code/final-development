47e7f3c0a6cd87090ef3126604c236657c01925f
sousuke-code <162286022+sousuke-code@users.noreply.github.com>
2024-06-26 16:33:36 +0900

    微調整2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,20 +78,7 @@ class UserController extends Controller
         return view('users.profileshow',['portfolios'=>$portfolios, 'user'=> $user]);
     }
 
-    public function destroy($id){
-    // 現在ログインしているユーザーを取得
-    $user = Auth::user();
-
-    // ユーザー情報を空にして保存
-    $user->name = null;
-    $user->email = null;
-    $user->bio = null;
-    $user->career = null;
-    $user->save(); 
-
-    // プロフィール表示ページにリダイレクト
-    return redirect()->route('users.show',$user->id);
-}
+ 
 
     public function destroy($id){
     // 現在ログインしているユーザーを取得
